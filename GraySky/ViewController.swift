2bd23b5ec45b967570d946c81c493ddaa02c6801
umutkonmus <umutkonmus@gmail.com>
2025-01-11 01:10:12 +0300

    Initial Commit

---
+++ GraySky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GraySky
+//
+//  Created by Umut Konmuş on 11.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GraySkyTests/GraySkyTests.swift
@@ -0,0 +1,17 @@
+//
+//  GraySkyTests.swift
+//  GraySkyTests
+//
+//  Created by Umut Konmuş on 11.01.2025.
+//
+
+import Testing
+@testable import GraySky
+
+struct GraySkyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
